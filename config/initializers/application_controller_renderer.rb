7d5abf5a7014f6e508a26e952cc14c38f70f9466
kazu-png <kazuki.t.1006@gmail.com>
2021-05-02 15:27:54 +0000

    [Finish]7-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'e6838cb0001d85f1e54fbdb1fadebb9661b6fd27b60094c565691d747e78167842fbef95d664ebd36e3e887fdb77f572606b00c3533bd41df01bf34aea4b8514'
