411d9bbc8638a79fb6fe3c525be20e7798ba4c6a
kazu-png <kazuki.t.1006@gmail.com>
2021-05-06 15:38:44 +0000

    [Finish]完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'homes#top'
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
-end
+  root 'post_images#index'
+  resources :post_images, only: [:new, :create, :index, :show] do
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
+  # 以下の行のonly: []内にupdateを追加
+  resources :users, only: [:show, :edit, :update]end
